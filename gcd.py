c6d8583434bdc800bbff1f0b778d7efe0bff9192
Binoy Barnabas <77873811+binoybarnabas@users.noreply.github.com>
2021-11-30 16:06:16 +0530

    Add files via upload

---
+++ gcd.py
@@ -0,0 +1,16 @@
+#gcd
+
+def gcd(a,b):
+    if a == 0 :
+        return b
+    if b==0:
+        return a
+    if a==b:
+        return a
+    if a>b :
+        return gcd(a-b,b)
+        return gcd(b-a,a)
+
+a,b = int(input("enter the value of a")),int(input("enter the value of a"))
+gcdval = gcd(a,b)
+print("gcd(",a,",",b,")=",gcdval)
