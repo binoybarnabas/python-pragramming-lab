fdfbb9dd13a7a5acbee2dad067785978909fc2bd
Binoy Barnabas <77873811+binoybarnabas@users.noreply.github.com>
2021-12-03 11:11:54 +0530

    Add files via upload

---
+++ co2.4.py
@@ -0,0 +1,29 @@
+#Generate a list of four digit numbers in a given range with all their digits even and the
+#number is a perfect square.
+import math
+
+start,limit = int(input("4 digit number start:")),int(input("4 digit number limit:"))
+even = [2,4,6,8,0]
+main = []
+count = 0
+for i in range(start,limit,1):
+      sr = math.sqrt(i)
+      if int(sr+0.5)**2 == i:
+          num = i
+          while(num>0):
+              d=num%10
+              if d%2 == 0:
+                  count = count+1
+              num = num//10
+          if count == 4:
+              main.append(i)
+      count = 0     
+        
+          
+
+print("list with even perfect squarea are",main)
+              
+              
+                  
+    
+
